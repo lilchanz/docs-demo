c57d40d0d3acdfd0088756b76efcb44d031e7dc6
lilchanz <1835857359@qq.com>
2024-08-13 15:10:21 +0800

    first commit

---
+++ .vitepress/config.mjs
@@ -0,0 +1,71 @@
+import { defineConfig } from 'vitepress'
+import { set_sidebar } from "./utils/auto_sidebar.mjs";	// 改成自己的路径
+
+// https://vitepress.dev/reference/site-config
+export default defineConfig({
+  title: "My Awesome Project",
+  description: "A VitePress Site",
+  themeConfig: {
+
+    outlineTitle:"目录",
+    outline:[2,6],
+    // https://vitepress.dev/reference/default-theme-config
+    nav: [
+      { text: '家', items:[
+      { text: '示例', link: '/markdown-examples' },
+      ],
+    },
+      { text: 'markdown示例',link: '/markdown-examples' },
+      { text: '自动生成1',link: '/front-end/react' },
+      { text: '自动生成2',link: '/backend/rabbitmq' },
+      { text: '两边栏演示',link: '/两边栏演示' },
+
+    ],
+
+    // sidebar: [
+    //   {
+    //     text: 'Examples',
+    //     items: [
+    //       { text: 'Markdown 演示', link: '/markdown-examples' },
+    //       { text: '运行 API 演示', link: '/api-examples' }
+    //     ]
+    //   }
+    // ],
+    // sidebar: {
+    //    "/front-end/react": set_sidebar("front-end/react") ,
+    //    "/backend/rabbitmq": set_sidebar("backend/rabbitmq") ,
+             
+
+    // },
+    sidebar: false, // 关闭侧边栏
+    aside: "left", // 设置右侧侧边栏在左侧显示
+
+    socialLinks: [
+      { icon: 'github', link: 'https://github.com/vuejs/vitepress' }
+    ],
+    footer: {
+       copyright:"Copyright@ 2024 Lil Chanz"
+    },
+       // 设置搜索框的样式
+       search: {
+        provider: "local",
+        options: {
+          translations: {
+            button: {
+              buttonText: "搜索文档",
+              buttonAriaLabel: "搜索文档",
+            },
+            modal: {
+              noResultsText: "无法找到相关结果",
+              resetButtonTitle: "清除查询条件",
+              footer: {
+                selectText: "选择",
+                navigateText: "切换",
+              },
+            },
+          },
+        },
+      },
+  },
+
+})
---
+++ index.md
@@ -0,0 +1,25 @@
+---
+# https://vitepress.dev/reference/default-theme-home-page
+layout: home
+
+hero:
+  name: "李前振的文档项目"
+  text: "文档站点"
+  tagline: My great project tagline
+  actions:
+    - theme: brand
+      text: 示例
+      link: https://www.jd.come
+    - theme: API
+      text: API Examples
+      link: /api-examples
+
+features:
+  - title: Feature A
+    details: 特征1详细描述
+  - title: Feature B
+    details: 特征2详细描述
+  - title: Feature C
+    details: 特征3详细描述
+---
+
